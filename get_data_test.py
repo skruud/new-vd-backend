63d2e87c72e425430b055b1fd5a85865ebd0924c
skruud <skruud@gmail.com>
2021-03-11 23:32:46 +0100

    Update get_data_test.py

--- get_data_test.py
+++ get_data_test.py
@@ -15,12 +15,28 @@ json_test = {
         "Host": "xxx.us-east-2.amazonaws.com",
         "User-Agent": "Mozilla/5.0"
     },
-    "body": {
+    "body": [
+        {
+        "type": "normall",
         "select": "amount, date",
         "from": "occupation",
         "where": "location = 'Oslo' AND category = 'IT utvikling'",
         "order": "date"
-    }
+        },
+        {
+        "type": "normal",
+        "select": "amount, date",
+        "from": "occupation",
+        "where": "location = 'Oslo' AND category = 'IT utvikling'",
+        "order": "date"
+        }
+    ]
 }
 
-get_data(json_test, 'b')
+data = get_data(json_test, 'b')
+
+for element in data['body']:
+    print('Start of element:')
+    print(element)
+    #for amount, date in element:
+        #print(amount)
